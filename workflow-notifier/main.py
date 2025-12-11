392285cf39e42f499c947df7b5466c0450a93ffa
Thomas Sundvoll <35451859+tsundvoll@users.noreply.github.com>
2025-12-11 13:07:30 +0100

    Restructure and add test

--- workflow-notifier/main.py
+++ workflow-notifier/main.py
@@ -1,14 +1,6 @@
-import typer
-
-from workflow_notifier.anonymizer import app as anonymizer_app
-from workflow_notifier.cloe import app as cloe_app
-from workflow_notifier.fencilla import app as fencilla_app
+from workflow_notifier.app import make_app
 
 if __name__ == "__main__":
-    app = typer.Typer()
-
-    app.add_typer(anonymizer_app)
-    app.add_typer(cloe_app)
-    app.add_typer(fencilla_app)
+    app = make_app()
 
     app()
