130ce87cd534d09469f175f92274c247814c7f8d
Ola Alstad <OALST@equinor.com>
2025-12-23 11:30:12 +0100

    Fix workflow notifier adding thermal reading notifiers

--- workflow-notifier/src/workflow_notifier/app.py
+++ workflow-notifier/src/workflow_notifier/app.py
@@ -3,6 +3,7 @@ import typer
 from workflow_notifier.anonymizer import app as anonymizer_app
 from workflow_notifier.cloe import app as cloe_app
 from workflow_notifier.fencilla import app as fencilla_app
+from workflow_notifier.thermal_reading import app as thermal_reading_app
 
 
 def make_app():
@@ -11,5 +12,6 @@ def make_app():
     app.add_typer(anonymizer_app)
     app.add_typer(cloe_app)
     app.add_typer(fencilla_app)
+    app.add_typer(thermal_reading_app)
 
     return app
