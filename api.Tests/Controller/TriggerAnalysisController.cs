3d6e62621f3a95168428f92f5593bf465ebff433
mrica-equinor <mrica@equinor.com>
2026-01-22 13:37:16 +0100

    Add trigger of analysis in plant data

--- api.Tests/Controller/TriggerAnalysisController.cs
+++ api.Tests/Controller/TriggerAnalysisController.cs
@@ -28,7 +28,7 @@ namespace api.Controllers.Tests
         }
 
         [Fact]
-        public async Task TriggerAnalysis_ReturnsNotFound_WhenPlantDataDoesNotExist()
+        public async Task TriggerAnonymizer_ReturnsNotFound_WhenPlantDataDoesNotExist()
         {
             //Arrange
             var plantDataId = "nonexistent-id";
@@ -37,14 +37,14 @@ namespace api.Controllers.Tests
                 .ReturnsAsync((PlantData?)null);
 
             //Act
-            var result = await _triggerAnalysisController.TriggerAnalysis(plantDataId);
+            var result = await _triggerAnalysisController.TriggerAnonymizer(plantDataId);
 
             //Assert
             Assert.IsType<NotFoundObjectResult>(result);
         }
 
         [Fact]
-        public async Task TriggerAnalysis_ReturnsConflict_WhenWorkflowsAlreadyTriggered()
+        public async Task TriggerAnonymizer_ReturnsConflict_WhenWorkflowsAlreadyTriggered()
         {
             //Arrange
             var plantDataId = "dummy-id";
@@ -75,7 +75,7 @@ namespace api.Controllers.Tests
                 );
 
             //Act
-            var result = await _triggerAnalysisController.TriggerAnalysis(plantDataId);
+            var result = await _triggerAnalysisController.TriggerAnonymizer(plantDataId);
 
             //Assert
             Assert.IsType<ConflictObjectResult>(result);
@@ -87,7 +87,7 @@ namespace api.Controllers.Tests
         }
 
         [Fact]
-        public async Task TriggerAnalysis_TriggersWorkflow_WhenPlantDataExistsAndNotStarted()
+        public async Task TriggerAnonymizer_TriggersWorkflow_WhenPlantDataExistsAndNotStarted()
         {
             //Arrange
             var plantDataId = "dummy-id";
@@ -121,12 +121,214 @@ namespace api.Controllers.Tests
             );
 
             //Act
-            var result = await _triggerAnalysisController.TriggerAnalysis(plantDataId);
+            var result = await _triggerAnalysisController.TriggerAnonymizer(plantDataId);
 
             //Assert
             Assert.IsType<OkObjectResult>(result);
             var okResult = result as OkObjectResult;
             Assert.Equal("Anonymization workflow triggered successfully.", okResult!.Value);
+        }
+
+        [Fact]
+        public async Task TriggerAnalysis_ReturnsNotFound_WhenPlantDataDoesNotExist()
+        {
+            // Arrange
+            var plantDataId = "missing-id";
+
+            _plantDataServiceMock
+                .Setup(service => service.ReadById(plantDataId))
+                .ReturnsAsync((PlantData?)null);
+
+            // Act
+            var result = await _triggerAnalysisController.TriggerAnalysis(plantDataId);
+
+            // Assert
+            var notFound = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal($"Could not find plant data with id {plantDataId}", notFound.Value);
+        }
+
+        [Fact]
+        public async Task TriggerAnalysis_ReturnsOk_WhenServiceSucceeds()
+        {
+            // Arrange
+            var plantDataId = "dummy-id";
+
+            _plantDataServiceMock
+                .Setup(service => service.ReadById(plantDataId))
+                .ReturnsAsync(
+                    new PlantData
+                    {
+                        InspectionId = "inspection-id",
+                        InstallationCode = "dummy-installation-code",
+                        Anonymization = new Anonymization
+                        {
+                            DestinationBlobStorageLocation = new BlobStorageLocation
+                            {
+                                StorageAccount = "dummyStorageAccount",
+                                BlobContainer = "dummyBlobContainer",
+                                BlobName = "dummyBlobName",
+                            },
+                            SourceBlobStorageLocation = new BlobStorageLocation
+                            {
+                                StorageAccount = "dummySourceStorageAccount",
+                                BlobContainer = "dummySourceBlobContainer",
+                                BlobName = "dummySourceBlobName",
+                            },
+                        },
+                    }
+                );
+
+            // Act
+            var result = await _triggerAnalysisController.TriggerAnalysis(plantDataId);
+
+            // Assert
+            var ok = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(
+                "Triggering anonymization workflow which will trigger analysis workflows.",
+                ok.Value
+            );
+        }
+
+        [Fact]
+        public async Task TriggerAnalysis_ReturnsConflict_WhenAnonymizationStarted()
+        {
+            // Arrange
+            var plantDataId = "dummy-id";
+
+            _plantDataServiceMock
+                .Setup(service => service.ReadById(plantDataId))
+                .ReturnsAsync(
+                    new PlantData
+                    {
+                        InspectionId = "inspection-id",
+                        InstallationCode = "dummy-installation-code",
+                        Anonymization = new Anonymization
+                        {
+                            DestinationBlobStorageLocation = new BlobStorageLocation
+                            {
+                                StorageAccount = "dummyStorageAccount",
+                                BlobContainer = "dummyBlobContainer",
+                                BlobName = "dummyBlobName",
+                            },
+                            SourceBlobStorageLocation = new BlobStorageLocation
+                            {
+                                StorageAccount = "dummySourceStorageAccount",
+                                BlobContainer = "dummySourceBlobContainer",
+                                BlobName = "dummySourceBlobName",
+                            },
+                            Status = WorkflowStatus.Started,
+                        },
+                    }
+                );
+
+            // Act
+            var result = await _triggerAnalysisController.TriggerAnalysis(plantDataId);
+
+            // Assert
+            var conflict = Assert.IsType<ConflictObjectResult>(result);
+            Assert.Equal(
+                "Anonymization is still in progress. Analysis workflows will be triggered once it completes.",
+                conflict.Value
+            );
+        }
+
+        [Fact]
+        public async Task TriggerAnalysis_ReturnsOk_WhenAnonymizationExitSuccessAndNoConfiguredAnalyses()
+        {
+            // Arrange
+            var plantDataId = "dummy-id";
+
+            _plantDataServiceMock
+                .Setup(service => service.ReadById(plantDataId))
+                .ReturnsAsync(
+                    new PlantData
+                    {
+                        InspectionId = "inspection-id",
+                        InstallationCode = "dummy-installation-code",
+                        Anonymization = new Anonymization
+                        {
+                            DestinationBlobStorageLocation = new BlobStorageLocation
+                            {
+                                StorageAccount = "dummyStorageAccount",
+                                BlobContainer = "dummyBlobContainer",
+                                BlobName = "dummyBlobName",
+                            },
+                            SourceBlobStorageLocation = new BlobStorageLocation
+                            {
+                                StorageAccount = "dummySourceStorageAccount",
+                                BlobContainer = "dummySourceBlobContainer",
+                                BlobName = "dummySourceBlobName",
+                            },
+                            Status = WorkflowStatus.ExitSuccess,
+                        },
+                    }
+                );
+
+            // Act
+            var result = await _triggerAnalysisController.TriggerAnalysis(plantDataId);
+
+            // Assert
+            var ok = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(
+                $"No analysis workflows configured for plant data with Id {plantDataId}.",
+                ok.Value
+            );
+        }
+
+        [Fact]
+        public async Task TriggerAnalysis_ReturnsOk_WhenAnonymizationExitSuccessAndConfiguredCLOEAnalysis()
+        {
+            // Arrange
+            var plantDataId = "dummy-id";
+
+            _plantDataServiceMock
+                .Setup(service => service.ReadById(plantDataId))
+                .ReturnsAsync(
+                    new PlantData
+                    {
+                        InspectionId = "inspection-id",
+                        InstallationCode = "dummy-installation-code",
+                        Anonymization = new Anonymization
+                        {
+                            DestinationBlobStorageLocation = new BlobStorageLocation
+                            {
+                                StorageAccount = "dummyStorageAccount",
+                                BlobContainer = "dummyBlobContainer",
+                                BlobName = "dummyBlobName",
+                            },
+                            SourceBlobStorageLocation = new BlobStorageLocation
+                            {
+                                StorageAccount = "dummySourceStorageAccount",
+                                BlobContainer = "dummySourceBlobContainer",
+                                BlobName = "dummySourceBlobName",
+                            },
+                            Status = WorkflowStatus.ExitSuccess,
+                        },
+                        CLOEAnalysis = new CLOEAnalysis
+                        {
+                            SourceBlobStorageLocation = new BlobStorageLocation
+                            {
+                                StorageAccount = "dummySourceStorageAccount",
+                                BlobContainer = "dummySourceBlobContainer",
+                                BlobName = "dummySourceBlobName",
+                            },
+                            DestinationBlobStorageLocation = new BlobStorageLocation
+                            {
+                                StorageAccount = "dummyDestinationStorageAccount",
+                                BlobContainer = "dummyDestinationBlobContainer",
+                                BlobName = "dummyDestinationBlobName",
+                            },
+                            Status = WorkflowStatus.NotStarted,
+                        },
+                    }
+                );
+
+            // Act
+            var result = await _triggerAnalysisController.TriggerAnalysis(plantDataId);
+
+            // Assert
+            var ok = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal($"Triggered analysis workflows: CLOE analysis", ok.Value);
         }
     }
 }
