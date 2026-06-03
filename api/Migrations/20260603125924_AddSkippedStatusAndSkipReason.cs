4130c9cf5376f966ee147ebc9ff533556c8248b2
mrica-equinor <mrica@equinor.com>
2026-06-03 15:05:36 +0200

    Add migrations

---
+++ api/Migrations/20260603125924_AddSkippedStatusAndSkipReason.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSkippedStatusAndSkipReason : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SkipReason",
+                table: "AnalysisRuns",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SkipReason",
+                table: "AnalysisRuns");
+        }
+    }
+}
