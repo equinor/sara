ebf350f74fd5940317d6434cc672795abb086047
Christian de Jonge <chjo@equinor.com>
2026-07-31 11:07:00 +0200

    MQTT: Include output blob location in visualization_available payload

--- api/MQTT/IsarInspectionResult.cs
+++ api/MQTT/IsarInspectionResult.cs
@@ -106,6 +106,15 @@ public class SaraVisualizationAvailableMessage : MqttMessage
 
     [JsonPropertyName("analysis_id")]
     public required Guid AnalysisId { get; set; }
+
+    [JsonPropertyName("storageAccount")]
+    public required string StorageAccount { get; set; }
+
+    [JsonPropertyName("blobContainer")]
+    public required string BlobContainer { get; set; }
+
+    [JsonPropertyName("blobName")]
+    public required string BlobName { get; set; }
 }
 
 public class SaraAnalysisResultMessage : MqttMessage
