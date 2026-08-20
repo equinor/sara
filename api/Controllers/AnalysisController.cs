b0e1e98adec97470198f4e0a1243f140b9978caa
Nicholas Dalhaug <nicd@equinor.com>
2026-08-20 13:22:23 +0200

    Remove redundant endpoint

--- api/Controllers/AnalysisController.cs
+++ api/Controllers/AnalysisController.cs
@@ -75,32 +75,6 @@ public class AnalysisController(
         }
     }
 
-    [HttpGet]
-    [Authorize(Roles = Role.Any)]
-    [Route("run/{runId:guid}")]
-    [ProducesResponseType(typeof(AnalysisRun), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<AnalysisRun>> GetRunById([FromRoute] Guid runId)
-    {
-        try
-        {
-            var run = await analysisService.ReadRunById(runId);
-            if (run is null)
-            {
-                return NotFound($"Could not find analysis run with id {runId}");
-            }
-            return Ok(run);
-        }
-        catch (Exception e)
-        {
-            logger.LogError(e, "Error during GET of analysis run by id");
-            throw;
-        }
-    }
-
     [HttpPost]
     [Authorize(Roles = Role.Any)]
     [Route("id/{id:guid}/rerun")]
