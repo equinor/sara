75f0329c16440428cf3370c61aa4de2f38526350
Nicholas Dalhaug <nicd@equinor.com>
2026-08-21 08:47:10 +0200

    Add feedback migrations

---
+++ api/Migrations/20260821064357_AddFeedback.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFeedback : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AnalysisRunFeedbacks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AnalysisRunId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsCorrect = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnalysisRunFeedbacks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AnalysisRunFeedbacks_AnalysisRuns_AnalysisRunId",
+                        column: x => x.AnalysisRunId,
+                        principalTable: "AnalysisRuns",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnalysisRunFeedbacks_AnalysisRunId",
+                table: "AnalysisRunFeedbacks",
+                column: "AnalysisRunId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnalysisRunFeedbacks");
+        }
+    }
+}
