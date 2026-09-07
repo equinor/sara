a02b32d90c6ab95705c708eb4d697979c924efd3
Anders C Indrebø <aindreboe@gmail.com>
2026-09-07 15:55:42 +0200

    Add migration to rename ThermalMetadata table

---
+++ api/Migrations/20260907135200_RenameThermalMetadataToPolygonMetadata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameThermalMetadataToPolygonMetadata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(name: "ThermalReferenceMetadata", newName: "ReferencePolygonMetadata");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ThermalReferenceMetadata_InstallationCode_TagId_InspectionD~",
+                table: "ReferencePolygonMetadata",
+                newName: "IX_ReferencePolygonMetadata_InstallationCode_TagId_InspectionD~");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameIndex(
+                newName: "IX_ThermalReferenceMetadata_InstallationCode_TagId_InspectionD~",
+                table: "ReferencePolygonMetadata",
+                name: "IX_ReferencePolygonMetadata_InstallationCode_TagId_InspectionD~");
+
+            migrationBuilder.RenameTable(newName: "ThermalReferenceMetadata", name: "ReferencePolygonMetadata");
+        }
+    }
+}
