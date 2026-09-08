359adfed41ec6550cc77ca8ab2d5e82571009b12
Anders C Indrebø <aindreboe@gmail.com>
2026-09-08 12:17:28 +0200

    Add migrations for storing ref polygon in db

---
+++ api/Migrations/20260908101445_StoreReferencePolygonsInTheDatabase.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class StoreReferencePolygonsInTheDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferencePolygonBlobStorageLocation_BlobContainer",
+                table: "ReferencePolygonMetadata");
+
+            migrationBuilder.DropColumn(
+                name: "ReferencePolygonBlobStorageLocation_BlobName",
+                table: "ReferencePolygonMetadata");
+
+            migrationBuilder.RenameColumn(
+                name: "ReferencePolygonBlobStorageLocation_StorageAccount",
+                table: "ReferencePolygonMetadata",
+                newName: "Polygon");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Polygon",
+                table: "ReferencePolygonMetadata",
+                newName: "ReferencePolygonBlobStorageLocation_StorageAccount");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReferencePolygonBlobStorageLocation_BlobContainer",
+                table: "ReferencePolygonMetadata",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReferencePolygonBlobStorageLocation_BlobName",
+                table: "ReferencePolygonMetadata",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
